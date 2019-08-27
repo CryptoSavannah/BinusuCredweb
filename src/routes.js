9a3615ab63168d4956bda3c1d0bf1148f9764ce2
Allan Katongole <kapsonkatongole@gmail.com>
2019-08-27 11:27:54 +0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import Dashboard from "views/Dashboard.jsx";
+import UserProfile from "views/UserProfile.jsx";
+import TableList from "views/TableList.jsx";
+import Typography from "views/Typography.jsx";
+import Icons from "views/Icons.jsx";
+import Notifications from "views/Notifications.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/lenders",
+    name: "Lenders",
+    icon: "pe-7s-user",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/rankings",
+    name: "Rankings",
+    icon: "pe-7s-note2",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/forecasts",
+    name: "Forecasts",
+    icon: "pe-7s-news-paper",
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/admin_settings",
+    name: "Admin Configuration",
+    icon: "pe-7s-science",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "pe-7s-bell",
+    component: Notifications,
+    layout: "/admin"
+  },
+];
+
+export default dashboardRoutes;
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import ChartistGraph from "react-chartist";
+import { Grid, Row, Col } from "react-bootstrap";
+
+import { Card } from "components/Card/Card.jsx";
+import { StatsCard } from "components/StatsCard/StatsCard.jsx";
+import { Tasks } from "components/Tasks/Tasks.jsx";
+import {
+  dataPie,
+  legendPie,
+  dataSales,
+  optionsSales,
+  responsiveSales,
+  legendSales,
+  dataBar,
+  optionsBar,
+  responsiveBar,
+  legendBar
+} from "variables/Variables.jsx";
+
+class Dashboard extends Component {
+  createLegend(json) {
+    var legend = [];
+    for (var i = 0; i < json["names"].length; i++) {
+      var type = "fa fa-circle text-" + json["types"][i];
+      legend.push(<i className={type} key={i} />);
+      legend.push(" ");
+      legend.push(json["names"][i]);
+    }
+    return legend;
+  }
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-server text-warning" />}
+                statsText="Lending Pool"
+                statsValue="450M BNU"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-wallet text-success" />}
+                statsText="Total Credit"
+                statsValue="190M BNU"
+                statsIcon={<i className="fa fa-calendar-o" />}
+                statsIconText="Last day"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-cash text-danger" />}
+                statsText="Lenders"
+                statsValue="500"
+                statsIcon={<i className="fa fa-clock-o" />}
+                statsIconText="In the last hour"
+              />
+            </Col>
+            <Col lg={3} sm={6}>
+              <StatsCard
+                bigIcon={<i className="pe-7s-portfolio text-info" />}
+                statsText="Borrowers"
+                statsValue="450K"
+                statsIcon={<i className="fa fa-refresh" />}
+                statsIconText="Updated now"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={8}>
+              <Card
+                statsIcon="fa fa-history"
+                id="chartHours"
+                title="Credit Forecasts"
+                category="24 Hours performance"
+                stats="Updated 3 minutes ago"
+                content={
+                  <div className="ct-chart">
+                    <ChartistGraph
+                      data={dataSales}
+                      type="Line"
+                      options={optionsSales}
+                      responsiveOptions={responsiveSales}
+                    />
+                  </div>
+                }
+                legend={
+                  <div className="legend">{this.createLegend(legendSales)}</div>
+                }
+              />
+            </Col>
+            <Col md={4}>
+              <Card
+                statsIcon="fa fa-clock-o"
+                title="Credit Players"
+                category="24 Hours Performance"
+                stats="Campaign sent 2 days ago"
+                content={
+                  <div
+                    id="chartPreferences"
+                    className="ct-chart ct-perfect-fourth"
+                  >
+                    <ChartistGraph data={dataPie} type="Pie" />
+                  </div>
+                }
+                legend={
+                  <div className="legend">{this.createLegend(legendPie)}</div>
+                }
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={6}>
+              <Card
+                id="chartActivity"
+                title="Trend Analysis"
+                category="All loans including Taxes"
+                stats="Data information certified"
+                statsIcon="fa fa-check"
+                content={
+                  <div className="ct-chart">
+                    <ChartistGraph
+                      data={dataBar}
+                      type="Bar"
+                      options={optionsBar}
+                      responsiveOptions={responsiveBar}
+                    />
+                  </div>
+                }
+                legend={
+                  <div className="legend">{this.createLegend(legendBar)}</div>
+                }
+              />
+            </Col>
+
+            <Col md={6}>
+              <Card
+                title="Query Management"
+                category="Customer queries and questions"
+                stats="Updated 3 minutes ago"
+                statsIcon="fa fa-history"
+                content={
+                  <div className="table-full-width">
+                    <table className="table">
+                      <Tasks />
+                    </table>
+                  </div>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
